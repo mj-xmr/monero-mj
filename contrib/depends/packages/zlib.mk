e678d656a5afd60230d29b4a3785463d74a5d529
mj-xmr <mjxmr@protonmail.com>
2021-03-16 19:01:02 +0100

    zlib compiled in

--- contrib/depends/packages/zlib.mk
+++ contrib/depends/packages/zlib.mk
@@ -1,27 +1,14 @@
 package=zlib
 $(package)_version=1.2.11
 $(package)_download_path=https://zlib.net
-$(package)_file_name=zlib-$($(package)_version).tar.gz
+$(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
 
-define $(package)_set_vars
-$(package)_config_opts=--static
-$(package)_config_opts+=--prefix=$(host_prefix)
-endef
-
-define $(package)_config_cmds
-  CHOST=$(host) CC="$(host_prefix)/native/bin/$($(package)_cc) -target $(host) -fPIC -pipe" ARFLAGS="$($(host_os)_ARFLAGS)" AR="$($(host_os)_AR)" RANLIB="$(host_prefix)/native/bin/$($(package)_ranlib)" \
-    LIBTOOL="$(host_prefix)/native/bin/$($(package)_libtool)" \
- ./configure --static --prefix=$(host_prefix) --eprefix=$(host_prefix)
-  #CHOST=$HOST ./configure $($(package)_config_opts) AR_FLAGS=$($(package)_arflags)
-  #CHOST=$HOST ./configure $($(package)_config_opts)
-endef
-
+# Here all that we do, is copying the content of the package to a fixed directory,
+# so that Boost can find it.
 define $(package)_build_cmds
-  $(MAKE) ARFLAGS="$($(host_os)_ARFLAGS)"
+  mkdir -p $(BASEDIR)/sources/$(package) && cp * -rf $(BASEDIR)/sources/$(package)
 endef
 
-define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
-endef
+
 
