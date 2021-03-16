004b56be7e308d25cddf1c225b1889a7f72afc43
mj-xmr <mjxmr@protonmail.com>
2021-03-16 21:13:56 +0100

    Boost zlib installed

--- contrib/depends/packages/boost.mk
+++ contrib/depends/packages/boost.mk
@@ -4,18 +4,15 @@ $(package)_download_path=https://dl.bintray.com/boostorg/release/1.64.0/source/
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
 $(package)_sha256_hash=7bcc5caace97baa948931d712ea5f37038dbb1c5d89b43ad4def4ed7cb683332
 $(package)_dependencies=libiconv
-$(package)_dependencies+=zlib
 $(package)_patches=fix_aroptions.patch
-zlib_src="$(BASEDIR)/sources/zlib"
 
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
 $(package)_config_opts_debug=variant=debug
 $(package)_config_opts=--layout=tagged --build-type=complete --user-config=user-config.jam
 $(package)_config_opts+=threading=multi link=static -sNO_BZIP2=1
-$(package)_config_opts+=-sZLIB_SOURCE="$(zlib_src)"
-$(package)_config_opts+=-sZLIB_INCLUDE="$(zlib_src)"
-$(package)_config_opts+=-sZLIB_LIBPATH="$(zlib_src)"
+$(package)_config_opts+=-sZLIB_INCLUDE="$(build_prefix)/usr/include"
+$(package)_config_opts+=-sZLIB_LIBPATH="$(build_prefix)/usr/lib"
 $(package)_config_opts_linux=threadapi=pthread runtime-link=shared
 $(package)_config_opts_android=threadapi=pthread runtime-link=static target-os=android
 $(package)_config_opts_darwin=--toolset=darwin-4.2.1 runtime-link=shared
--- contrib/depends/packages/packages.mk
+++ contrib/depends/packages/packages.mk
@@ -1,4 +1,4 @@
-packages:=boost openssl zeromq libiconv zlib
+packages:=boost openssl zeromq libiconv
 
 native_packages := native_ccache
 
