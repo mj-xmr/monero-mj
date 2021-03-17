c0b65c7293e36bb48f2c83b8e45b5106505b479f
mj-xmr <mjxmr@protonmail.com>
2021-03-17 11:53:39 +0100

    PIC for zlib

--- contrib/depends/packages/boost.mk
+++ contrib/depends/packages/boost.mk
@@ -20,11 +20,13 @@ $(package)_config_opts_mingw32=binary-format=pe target-os=windows threadapi=win3
 $(package)_config_opts_x86_64_mingw32=address-model=64
 $(package)_config_opts_i686_mingw32=address-model=32
 $(package)_config_opts_i686_linux=address-model=32 architecture=x86
+$(package)_config_opts_linux=cflags=-fPIC
+$(package)_config_opts_freebsd=cflags=-fPIC
 $(package)_toolset_$(host_os)=gcc
 $(package)_archiver_$(host_os)=$($(package)_ar)
 $(package)_toolset_darwin=darwin
 $(package)_archiver_darwin=$($(package)_libtool)
-$(package)_config_libraries=chrono,filesystem,program_options,system,thread,test,date_time,regex,serialization,locale,iostreams
+$(package)_config_libraries=chrono,filesystem,program_options,system,math,thread,test,date_time,regex,serialization,locale,iostreams
 $(package)_cxxflags=-std=c++11
 $(package)_cxxflags_linux=-fPIC
 $(package)_cxxflags_freebsd=-fPIC
